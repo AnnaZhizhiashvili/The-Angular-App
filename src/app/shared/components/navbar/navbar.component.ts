4b930a455981c826c6e2534fcc6deaa8b9828343
AnZhizhiashvili <anzhizhiashvili@tbcbank.com.ge>
2022-10-19 00:38:12 +0400

    feat - add navigation and customize material theme

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { logOutAction } from '../../../auth/store/actions/login.action';
-import { Store } from '@ngrx/store';
+import {
+  loginFailureAction,
+  loginSuccessAction,
+  logOutAction,
+} from '../../../auth/store/actions/login.action';
+import { select, Store } from '@ngrx/store';
+import { CurrentUserInterface } from '@shared/types/currentUser.interface';
+import { Observable } from 'rxjs';
+import { currentUserSelector } from '../../../auth/store/selectors';
+import { AuthService } from '../../../auth/services/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../../../auth/components/login/login.component';
 
 @Component({
   selector: 'app-navbar',
@@ -9,13 +19,32 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor(private router: Router, private store: Store) {}
-
-  ngOnInit(): void {}
+  currentUser$: Observable<CurrentUserInterface | null>;
+  constructor(
+    private router: Router,
+    private store: Store,
+    private auth: AuthService,
+    private dialog: MatDialog
+  ) {}
+  ngOnInit(): void {
+    const currentUser = this.auth.getFromLocalStorage('currentUser');
+    if (currentUser) {
+      this.store.dispatch(loginSuccessAction({ currentUser }));
+    } else {
+      this.store.dispatch(loginFailureAction());
+    }
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
+  }
 
   logOut() {
     this.router.navigateByUrl('login').then();
     this.store.dispatch(logOutAction());
     window.localStorage.clear();
+  }
+
+  openLogin() {
+    this.dialog.open(LoginComponent, {
+      width: '500px',
+    });
   }
 }
